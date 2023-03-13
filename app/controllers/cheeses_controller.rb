385513d977f35df50a130cf7de038853ef062522
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-03-13 20:30:34 +0300

    Complete Code Along

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,34 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
+# rather than specifically listing every key we want to include, we could also exclude particular content using the except:
+  # def index
+  # cheeses = Cheese.all
+  # render json: cheeses, except: [:created_at, :updated_at]
+  # end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #    id: cheese.id,
+  #    name: cheese.name,
+  #    price: cheese.price,
+  #    is_best_seller: cheese.is_best_seller
+  #  }
+  # end
+  # def show
+  #  cheese = Cheese.find_by(id: params[:id])
+  #  render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  # end
+def show
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    render json: { error: 'Cheese not found' }, status: :not_found
   end
-
+end
 end
